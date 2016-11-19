8c6dcd7f48455246ee5600b0ab4121fa9546a855
sontx <xuanson33bk@gmail.com>
2016-11-19 16:52:11 +0700

    ! init camera project(record a video from webcam)

---
+++ Camera/MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Camera
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cameraControl = new DevExpress.XtraEditors.Camera.CameraControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSave = new DevExpress.XtraEditors.SimpleButton();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cameraControl
+            // 
+            this.cameraControl.Cursor = System.Windows.Forms.Cursors.Default;
+            this.cameraControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cameraControl.Location = new System.Drawing.Point(0, 0);
+            this.cameraControl.Name = "cameraControl";
+            this.cameraControl.Size = new System.Drawing.Size(284, 222);
+            this.cameraControl.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 222);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 40);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(197, 8);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save...";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.cameraControl);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "Webcam";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.Camera.CameraControl cameraControl;
+        private System.Windows.Forms.Panel panel1;
+        private DevExpress.XtraEditors.SimpleButton btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
